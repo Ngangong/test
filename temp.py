180e9b78e0aa8c0b1a048ec9a6a13107c1df3515
Ngangong <ntngathanh@gmail.com>
2024-06-07 11:16:34 +0700

    Add files via upload

---
+++ temp.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import streamlit as st
+st.title("chương trình streamlit")
+st.write("đây là ứng dụng streamlit đầu tiên của bạn")
+st.text("Ứng dụng này đơn giản chỉ in ra một dòng văn bản")
+st.write(123)
+st.write(["item 1", "item 2", "item 3"])
+st.write({"cột 1": [1, 2, 3], "cột 2": [4, 5, 6]})
